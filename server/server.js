adcda6b2a9cd8797fa6f45e39e78193d1721f2d1
Hack Reactor Students <maseh87@gmail.com>
2014-06-24 16:21:51 -0700

    added a log file to save messages

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
-var express = require('express');
+var express = require('express')
+  , fs = require('fs');
 var bodyParser = require('body-parser');
 var app = express();
 var storage = {};
 storage.results = [];
+var logfile = fs.createWriteStream('./logfile.log', {flags: 'a'});
+// app.use(express.logger({stream: logfile}));
+
 
 app.use(express.static('../client/client'));
 app.use(bodyParser.json());
 app.post('/classes/messages', function(req, res){
   var temp = req.body;
   temp.createdAt = new Date();
+  fs.appendFile('./logfile.log', JSON.stringify(temp));
   storage.results.push(req.body);
   res.send(201);
-
 });
 app.get('/classes/messages', function(req, res){
   res.send(storage);
